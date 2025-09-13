bcbece2c05f6fa7d131cd695a560dc858a3d0a6a
AlanBP18 <142277977+AlanBP18@users.noreply.github.com>
2025-09-13 00:23:30 -0500

    cambios de practica

--- frontend-Angular/AngularFrontEnd/src/app/app.ts
+++ frontend-Angular/AngularFrontEnd/src/app/app.ts
@@ -1,11 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FirstPage } from './first-page/first-page';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FirstPage],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+
 })
 export class App {
   protected readonly title = signal('AngularFrontEnd');
