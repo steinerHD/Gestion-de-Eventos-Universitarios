16e9ea6c3a6532ff595230239f9dcb1008a683af
AlanBP18 <142277977+AlanBP18@users.noreply.github.com>
2025-09-19 16:53:58 -0500

    Prototipo Organizacion externa

--- frontend-Angular/AngularFrontEnd/src/app/app.ts
+++ frontend-Angular/AngularFrontEnd/src/app/app.ts
@@ -1,16 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { OrganizacionExterna } from './organizacion-externa/organizacion-externa';
+import { OrganizacionExternaComponent } from './organizacion-externa/organizacion-externa';
 import { first } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, OrganizacionExterna],
+  imports: [RouterOutlet, CommonModule, OrganizacionExternaComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 
 })
 export class App {
   protected readonly title = signal('AngularFrontEnd');
+    // Señal para gestionar el estado de la ventana emergente.
+  // Es `false` por defecto, lo que significa que la ventana está cerrada.
+  isModalOpen = signal(false);
+
+  // Método para abrir la ventana emergente
+  openModal(): void {
+    this.isModalOpen.set(true);
+  }
+
+  // Método para cerrar la ventana emergente
+  closeModal(): void {
+    this.isModalOpen.set(false);
 }
+}
+
+  standalone: true
--- frontend-Angular/AngularFrontEnd/src/app/organizacion-externa/organizacion-externa.ts
+++ frontend-Angular/AngularFrontEnd/src/app/organizacion-externa/organizacion-externa.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-organizacion-externa',
@@ -6,6 +6,15 @@ import { Component } from '@angular/core';
   templateUrl: './organizacion-externa.html',
   styleUrl: './organizacion-externa.css'
 })
-export class OrganizacionExterna {
 
+export class OrganizacionExternaComponent {
+  // Datos de ejemplo para la lista de organizaciones
+  organizaciones = signal([
+    { id: 1, nombre: 'Universidad Operativa de Colombia', nit: '12231341424151' },
+    { id: 2, nombre: 'Mercá', nit: '12231341734151' },
+    { id: 3, nombre: 'BVVA', nit: '1333331734151' },
+    { id: 4, nombre: 'Disneyland', nit: '13338461254151' },
+    { id: 5, nombre: 'Plan padrinos', nit: '1333327455151' },
+    { id: 6, nombre: 'Ministerio de la seriedad', nit: '13323987455151' }
+  ]);
 }
